3e21a7d941afd23aa52378d0dd296c6f66c50515
Eldar-Ka <eldark@post.bgu.ac.il>
2022-01-01 21:50:18 +0200

    Initial commit+page rank

---
+++ search_frontend.py
@@ -0,0 +1,242 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pandas as pd
+#%load_ext google.colab.data_table
+import bz2
+from functools import partial
+from collections import Counter, OrderedDict
+import pickle
+import heapq
+from itertools import islice, count, groupby
+from xml.etree import ElementTree
+import codecs
+import csv
+import os
+import re
+import gzip
+from operator import itemgetter
+import nltk
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from pathlib import Path
+import itertools
+from time import time
+import hashlib
+
+class MyFlaskApp(Flask):
+    def run(self, host=None, port=None, debug=None, **options):
+        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, **options)
+
+app = MyFlaskApp(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
+@app.route("/search")
+def search():
+    ''' Returns up to a 100 of your best search results for the query. This is 
+        the place to put forward your best search engine, and you are free to
+        implement the retrieval whoever you'd like within the bound of the 
+        project requirements (efficiency, quality, etc.). That means it is up to
+        you to decide on whether to use stemming, remove stopwords, use 
+        PageRank, query expansion, etc.
+
+        To issue a query navigate to a URL like:
+         http://YOUR_SERVER_DOMAIN/search?query=hello+world
+        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of up to 100 search results, ordered from best to worst where each 
+        element is a tuple (wiki_id, title).
+    '''
+    res = []
+    query = request.args.get('query', '')
+    if len(query) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+
+    # END SOLUTION
+    return jsonify(res)
+
+@app.route("/search_body")
+def search_body():
+    ''' Returns up to a 100 search results for the query using TFIDF AND COSINE
+        SIMILARITY OF THE BODY OF ARTICLES ONLY. DO NOT use stemming. DO USE the 
+        staff-provided tokenizer from Assignment 3 (GCP part) to do the 
+        tokenization and remove stopwords. 
+
+        To issue a query navigate to a URL like:
+         http://YOUR_SERVER_DOMAIN/search_body?query=hello+world
+        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of up to 100 search results, ordered from best to worst where each 
+        element is a tuple (wiki_id, title).
+    '''
+    res = []
+    query = request.args.get('query', '')
+    if len(query) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+
+    # END SOLUTION
+    return jsonify(res)
+
+@app.route("/search_title")
+def search_title():
+    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD 
+        IN THE TITLE of articles, ordered in descending order of the NUMBER OF 
+        QUERY WORDS that appear in the title. For example, a document with a 
+        title that matches two of the query words will be ranked before a 
+        document with a title that matches only one query term. 
+
+        Test this by navigating to the a URL like:
+         http://YOUR_SERVER_DOMAIN/search_title?query=hello+world
+        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of ALL (not just top 100) search results, ordered from best to 
+        worst where each element is a tuple (wiki_id, title).
+    '''
+    res = []
+    query = request.args.get('query', '')
+    if len(query) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+
+    # END SOLUTION
+    return jsonify(res)
+
+@app.route("/search_anchor")
+def search_anchor():
+    ''' Returns ALL (not just top 100) search results that contain A QUERY WORD 
+        IN THE ANCHOR TEXT of articles, ordered in descending order of the 
+        NUMBER OF QUERY WORDS that appear in anchor text linking to the page. 
+        For example, a document with a anchor text that matches two of the 
+        query words will be ranked before a document with anchor text that 
+        matches only one query term. 
+
+        Test this by navigating to the a URL like:
+         http://YOUR_SERVER_DOMAIN/search_anchor?query=hello+world
+        where YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of ALL (not just top 100) search results, ordered from best to 
+        worst where each element is a tuple (wiki_id, title).
+    '''
+    res = []
+    query = request.args.get('query', '')
+    if len(query) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+    
+    # END SOLUTION
+    return jsonify(res)
+
+@app.route("/get_pagerank", methods=['POST'])
+def get_pagerank():
+    ''' Returns PageRank values for a list of provided wiki article IDs. 
+
+        Test this by issuing a POST request to a URL like:
+          http://YOUR_SERVER_DOMAIN/get_pagerank
+        with a json payload of the list of article ids. In python do:
+          import requests
+          requests.post('http://YOUR_SERVER_DOMAIN/get_pagerank', json=[1,5,8])
+        As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of floats:
+          list of PageRank scores that correrspond to the provided article IDs.
+    '''
+    res = []
+    wiki_ids = request.get_json()
+    if len(wiki_ids) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+    lst=pg_table()
+    # END SOLUTION
+    return jsonify(res)
+
+@app.route("/get_pageview", methods=['POST'])
+def get_pageview():
+    ''' Returns the number of page views that each of the provide wiki articles
+        had in August 2021.
+
+        Test this by issuing a POST request to a URL like:
+          http://YOUR_SERVER_DOMAIN/get_pageview
+        with a json payload of the list of article ids. In python do:
+          import requests
+          requests.post('http://YOUR_SERVER_DOMAIN/get_pageview', json=[1,5,8])
+        As before YOUR_SERVER_DOMAIN is something like XXXX-XX-XX-XX-XX.ngrok.io
+        if you're using ngrok on Colab or your external IP on GCP.
+    Returns:
+    --------
+        list of ints:
+          list of page view numbers from August 2021 that correrspond to the 
+          provided list article IDs.
+    '''
+    res = []
+    wiki_ids = request.get_json()
+    if len(wiki_ids) == 0:
+      return jsonify(res)
+    # BEGIN SOLUTION
+
+    # END SOLUTION
+    return jsonify(res)
+def pg_table():
+    pages_links = spark.read.parquet(path).limit(1000).select("id", "anchor_text").rdd
+    def help(id, anchor):
+        dic = {}
+        for elem in anchor:
+            dic[elem[0]] = 1
+        lst = []
+        for key in dic:
+            lst.append((id, key))
+        return lst
+
+    def generate_graph(pages):
+        ''' Compute the directed graph generated by wiki links.
+        Parameters:
+        -----------
+          pages: RDD
+            An RDD where each row consists of one wikipedia articles with 'id' and
+            'anchor_text'.
+        Returns:
+        --------
+          edges: RDD
+            An RDD where each row represents an edge in the directed graph created by
+            the wikipedia links. The first entry should the source page id and the
+            second entry is the destination page id. No duplicates should be present.
+          vertices: RDD
+            An RDD where each row represents a vetrix (node) in the directed graph
+            created by the wikipedia links. No duplicates should be present.
+        '''
+        edges = pages.flatMap(lambda x: help(x[0], x[1]))
+        vertices1 = edges.flatMap(lambda x: (x[0], x[1]))
+        vertices = vertices1.distinct()
+        vertices = vertices.map(lambda x: [x])
+        return edges, vertices
+
+    # construct the graph for a small sample of (1000) pages
+    edges, vertices = generate_graph(pages_links)
+    # time the actual execution
+    v_cnt, e_cnt = vertices.count(), edges.count()
+    edgesDF = edges.toDF(['src', 'dst']).repartition(4, 'src')
+    verticesDF = vertices.toDF(['id']).repartition(4, 'id')
+    g = GraphFrame(verticesDF, edgesDF)
+    pr_results = g.pageRank(resetProbability=0.15, maxIter=10)
+    pr = pr_results.vertices.select("id", "pagerank")
+    pr = pr.sort(col('pagerank').desc())
+    pr.repartition(1).write.csv('pr', compression="gzip")
+    pr.show()
+    return pr
+
+if __name__ == '__main__':
+    # run the Flask RESTful API, make the server publicly available (host='0.0.0.0') on port 8080
+    app.run(host='0.0.0.0', port=8080, debug=True)
